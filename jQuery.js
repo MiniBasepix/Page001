6d64f190d76649e92ee656a881219abadb248f06
Clos Jasl <85016704+MiniBasepix@users.noreply.github.com>
2021-10-21 18:04:17 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -2,3 +2,11 @@ window.onload = function() {
     const spinner = document.getElementById('loading');
     spinner.classList.add('loaded');
   }
+
+$(function(){
+  setTimeout('stopload()',10000);
+});
+  
+function stopload(){
+  $('.loaded').delay(600).fadeOut(300);
+}
